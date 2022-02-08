4bba41c2bde3f147c39c39a37285991abba3a19b
Shrinika02 <85934861+Shrinika02@users.noreply.github.com>
2022-02-08 16:01:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -35,3 +35,75 @@ function new_img(get_img){
         canvas.add(block_object);
     })
 }
+window.addEventListener("keydown", my_keypressed);
+function my_keypressed(e){
+    keypressed=e.keyCode;
+     if (e.shiftKey && keypressed=="80"){
+         block_img_height=block_img_height+10;
+         block_img_width=block_img_width+10
+         document.getElementById("width").innerHTML=block_img_width;
+         document.getElementById("height").innerHTML=block_img_height;
+     }
+     if (e.shiftKey && keypressed=="77"){
+        block_img_height=block_img_height-10;
+        block_img_width=block_img_width-10
+        document.getElementById("width").innerHTML=block_img_width;
+        document.getElementById("height").innerHTML=block_img_height;
+    }
+    
+    if (keypressed=="38"){
+        console.log("up");
+       up();
+    }
+    if (keypressed=="37"){
+        console.log("left");
+       left();
+    }
+    if (keypressed=="39"){
+        console.log("right");
+    right();
+    }
+    if (keypressed=="40"){
+        console.log("down");
+       down();
+
+    }
+
+    if (keypressed=="87"){
+        new_img("wall.jpg")
+        console.log("w")
+    }
+    if (keypressed=="71"){
+        new_img("ground.png")
+        console.log("g")
+    }
+    if (keypressed=="68"){
+        new_img("dark_green.png")
+        console.log("d")
+    }
+    if (keypressed=="76"){
+        new_img("light_green.png")
+        console.log("l")
+    }
+    if (keypressed=="84"){
+        new_img("trunk.jpg")
+        console.log("t")
+    }
+    
+    if (keypressed=="82"){
+        new_img("roof.jpg")
+        console.log("r")
+    }
+    if (keypressed=="89"){
+        new_img("yellow_wall.png")
+        console.log("y")
+    }
+    if (keypressed=="85"){
+        new_img("unique.png")
+        console.log("u")
+    }
+    if (keypressed=="67"){
+        new_img("cloud.jpg")
+        console.log("c")
+    }
+}
